c5ab0262c8f9320ced6ec69cceed672d77e726c4
JanigianMarielle <158102962+JanigianMarielle@users.noreply.github.com>
2024-04-29 14:16:40 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -16,7 +16,7 @@ app.secret_key=os.environ["SECRET_KEY"]; #This is an environment variable.
 @app.route('/')
 def renderMain():
     return render_template('home.html')
-
+  
 @app.route('/startOver')
 def startOver():
     session.clear() #clears variable values and creates a new session
@@ -28,13 +28,18 @@ def renderPage1():
 
 @app.route('/page2',methods=['GET','POST'])
 def renderPage2():
-    session["firstName"]=request.form['firstName']
-    session["lastName"]=request.form['lastName']
+    session["firstname"]=request.form['firstname?']
+    session["favoriteColor"]=request.form['favoriteColor']
     return render_template('page2.html')
 
 @app.route('/page3',methods=['GET','POST'])
 def renderPage3():
-    session["favoriteColor"]=request.form['favoriteColor']
+    session["birthCity"]=request.form['What city were you born in?']
+    return render_template('page3.html')
+
+@app.route('/page3',methods=['GET','POST'])
+def renderPage3():
+    session["luckyNumber"]=request.form['luckyNumber']
     return render_template('page3.html')
     
 if __name__=="__main__":
